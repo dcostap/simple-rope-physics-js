4260e2b9f549d5f8220921950552d0e33cd82693
Dario Costa <dariocostapena@gmail.com>
2023-03-21 00:20:03 +0100

    Rope physics are a bit more accurate and less jittery

--- sketch.js
+++ sketch.js
@@ -1,105 +1,76 @@
+
+
 class Rope {
     constructor(numSegments, segmentLength, springConstant, damping) {
-        this.numSegments = numSegments;
-        this.segmentLength = segmentLength;
-        this.springConstant = springConstant;
-        this.damping = damping;
+      this.numSegments = numSegments;
+      this.segmentLength = segmentLength;
+      this.springConstant = springConstant;
+      this.damping = damping;
 
-        this.masses = [];
-        this.springs = [];
+      this.masses = [];
+      this.springs = [];
 
-        for (let i = 0; i < numSegments; i++) {
-            const mass = {
-                x: i * segmentLength,
-                y: 0,
-                vx: 0,
-                vy: 0,
-                ax: 0,
-                ay: 0
-            };
-            this.masses.push(mass);
+      for (let i = 0; i < numSegments; i++) {
+        const mass = {
+          x: i * segmentLength,
+          y: 0,
+          vx: 0,
+          vy: 0,
+          ax: 0,
+          ay: 0
+        };
+        this.masses.push(mass);
 
-            if (i > 0) {
-                const spring = {
-                    massA: this.masses[i - 1],
-                    massB: mass,
-                    restLength: segmentLength,
-                    springConstant: springConstant
-                };
-                this.springs.push(spring);
-            }
+        if (i > 0) {
+          const spring = {
+            massA: this.masses[i - 1],
+            massB: mass,
+            restLength: segmentLength,
+            springConstant: springConstant
+          };
+          this.springs.push(spring);
         }
-
-        this.anchor = { x: 0, y: 0 };
+      }
     }
 
     update() {
-        for (let i = 0; i < this.numSegments; i++) {
-            const mass = this.masses[i];
-            mass.x += mass.vx;
-            mass.y += mass.vy;
+      for (let i = 0; i < this.numSegments; i++) {
+        const mass = this.masses[i];
+        mass.x += mass.vx;
+        mass.y += mass.vy;
 
-            // Apply damping to the velocity of each mass
-            mass.vx *= 1 - this.damping;
-            mass.vy *= 1 - this.damping;
-        }
+        // Apply damping to the velocity of each mass
+        mass.vx *= 1 - this.damping;
+        mass.vy *= 1 - this.damping;
+      }
 
-        for (let i = 0; i < this.springs.length; i++) {
-            const spring = this.springs[i];
-            const dx = spring.massB.x - spring.massA.x;
-            const dy = spring.massB.y - spring.massA.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            const displacement = distance - spring.restLength;
-            const springForce = displacement * spring.springConstant;
-            const fx = (dx / distance) * springForce;
-            const fy = (dy / distance) * springForce;
-            spring.massA.ax += fx;
-            spring.massA.ay += fy;
-            spring.massB.ax -= fx;
-            spring.massB.ay -= fy;
-        }
-
-        for (let i = 0; i < this.numSegments; i++) {
-            const mass = this.masses[i];
-            mass.vx += mass.ax;
-            mass.vy += mass.ay;
-            mass.ax = 0;
-            mass.ay = 0;
-        }
-    }
-
-    place(x, y) {
-        // Set the anchor point to the specified position
-        this.anchor.x = x;
-        this.anchor.y = y;
-
-        // Reset the position of the masses to form a straight line
-        for (let i = 0; i < this.numSegments; i++) {
-            this.masses[i].x = this.anchor.x + i *
-                this.segmentLength;
-            this.masses[i].y = this.anchor.y;
-        }
-    }
-
-    dragTo(x, y, maxLength) {
-        // Calculate the distance between the anchor point and the specified point
-        let dx = x - this.anchor.x;
-        let dy = y - this.anchor.y;
+      for (let i = 0; i < this.springs.length; i++) {
+        const spring = this.springs[i];
+        const dx = spring.massB.x - spring.massA.x;
+        const dy = spring.massB.y - spring.massA.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
+        const displacement = distance - spring.restLength;
+        const springForce = displacement * spring.springConstant;
+        const fx = (dx / distance) * springForce;
+        const fy = (dy / distance) * springForce;
+        spring.massA.ax += fx;
+        spring.massA.ay += fy;
+        spring.massB.ax -= fx;
+        spring.massB.ay -= fy;
+      }
 
-        // Limit the maximum length of the rope
-        if (distance > maxLength) {
-            dx *= maxLength / distance;
-            dy *= maxLength / distance;
-        }
+      for (let i = 0; i < this.numSegments; i++) {
+        const mass = this.masses[i];
+        mass.vx += mass.ax;
+        mass.vy += mass.ay;
+        mass.ax = 0;
+        mass.ay = 0;
+      }
+    }
 
-        // Move the end of the rope to the specified point
-        this.masses[this.numSegments - 1].x = this.anchor.x + dx;
-        this.masses[this.numSegments - 1].y = this.anchor.y + dy;
-
-        // Apply a velocity to the end of the rope to simulate dragging
-        this.masses[this.numSegments - 1].vx = dx * 0.5;
-        this.masses[this.numSegments - 1].vy = dy * 0.5;
+    moveEnd(x, y) {
+        this.masses[0].x = x;
+        this.masses[0].y = y;
     }
 
     draw(x, y) {
@@ -108,8 +79,8 @@ class Rope {
         stroke(237, 222, 222);
         strokeWeight(1);
 
-        vertex(x + this.anchor.x, y + this.anchor.y);
-        for (let i = 0; i < this.numSegments; i++) {
+        vertex(x + this.masses[0].x, y + this.masses[0].y);
+        for (let i = 1; i < this.numSegments; i++) {
             vertex(x + this.masses[i].x, y + this.masses[i].y);
         }
         endShape();
@@ -215,19 +186,17 @@ const rope2 = new Rope(100, 1, 0.01, 0.1);
 
 function setup() {
     createCanvas(windowWidth / 1.1, windowHeight / 1.1);
-    rope.place(40, 50)
-    rope2.place(20, windowHeight)
 }
 
 function draw() {
     background(5);
 
     rope.update(0.5);
-    rope.dragTo(player.x, player.y, 500)
+    rope.moveEnd(player.x, player.y)
     rope.draw(0, 0);
 
     rope2.update(0.5);
-    rope2.dragTo(mouseX, mouseY, 1000)
+    rope2.moveEnd(mouseX, mouseY)
     rope2.draw(0, 0);
 
     player.update(walls);
